38f1cbd0a364465bcecfc9b976f870429c467898
SREYNAJ KEB <101303611+Sreynaj@users.noreply.github.com>
2022-11-15 23:39:32 +0700

    sourcecode

---
+++ Day2-Data Type/Day2-Exercise/level2.js
@@ -0,0 +1,16 @@
+//#1
+console.log("The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.")
+//#2
+console.log("Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.")
+//#3
+typeOf = '10'
+console.log(typeOf == 10)
+
+//#4
+parseFloat('9.8')
+console.log(parseFloat == 10)
+
+//#5 
+let py = 'Python'
+let jar = 'Jargon'
+console.log(py.search('on'))
