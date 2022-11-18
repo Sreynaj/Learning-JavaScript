96d7c095c5d19d6767fb4f65ff4dd2013dd4faa7
SREYNAJ KEB <101303611+Sreynaj@users.noreply.github.com>
2022-11-18 15:21:05 +0700

    sourcecode

--- Day3-Booleans-Operators-Date/Day3-Exercises/level2.js
+++ Day3-Booleans-Operators-Date/Day3-Exercises/level2.js
@@ -14,3 +14,19 @@ let b = parseInt(prompt('Enter side b: '))
 let c = parseInt(prompt('Enter side c: '))
 let P = a+b+c
 console.log("The perimeter of the triangle is: ",P)
+
+//#3
+console.log('\n---Calculate the Perimater of the triagle ---')
+let length = parseInt(prompt('Enter length: '))
+let weight = parseInt(prompt('Enter weight: '))
+let p = 2*(length+weight)
+console.log("The perimeter of the triangle is: ",p)
+
+//#4
+console.log('\n---Calculate the Area and Circumference of the cricle ---')
+let radius = parseInt(prompt('Enter the redius: '))
+let PI = 3.14
+let Area = PI * radius * radius
+let Circumference = 2 * PI * radius
+console.log("The area of the circle is : ",Area)
+console.log("The circumference of the circle is : ",Circumference)
